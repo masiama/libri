a3ed65768d2e226868d62a694e36ed86af8226f6
Leonid Cupikov <leonid.cupikov@gmail.com>
2023-07-04 17:30:21 +0300

    Setup linter

--- api/src/utils/prisma.ts
+++ api/src/utils/prisma.ts
@@ -1,18 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
-	var prisma: PrismaClient;
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient;
 }
 
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
+  prisma = new PrismaClient();
 } else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
-	prisma = global.prisma;
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
